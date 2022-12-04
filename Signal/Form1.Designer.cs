e10a97953ac3cc6ecdd48d4735eb6227b4166bbd
JINWOO <dorson2@naver.com>
2022-12-05 08:05:11 +0900

    Signal_2_by winform

--- Signal/Form1.Designer.cs
+++ Signal/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LampRed = new System.Windows.Forms.Button();
+            this.LampYellow = new System.Windows.Forms.Button();
+            this.LampGreen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LampRed
+            // 
+            this.LampRed.Location = new System.Drawing.Point(115, 37);
+            this.LampRed.Name = "LampRed";
+            this.LampRed.Size = new System.Drawing.Size(112, 104);
+            this.LampRed.TabIndex = 0;
+            this.LampRed.Text = "button1";
+            this.LampRed.UseVisualStyleBackColor = true;
+            // 
+            // LampYellow
+            // 
+            this.LampYellow.Location = new System.Drawing.Point(115, 158);
+            this.LampYellow.Name = "LampYellow";
+            this.LampYellow.Size = new System.Drawing.Size(112, 104);
+            this.LampYellow.TabIndex = 1;
+            this.LampYellow.Text = "button2";
+            this.LampYellow.UseVisualStyleBackColor = true;
+            // 
+            // LampGreen
+            // 
+            this.LampGreen.Location = new System.Drawing.Point(115, 279);
+            this.LampGreen.Name = "LampGreen";
+            this.LampGreen.Size = new System.Drawing.Size(112, 104);
+            this.LampGreen.TabIndex = 2;
+            this.LampGreen.Text = "button3";
+            this.LampGreen.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LampGreen);
+            this.Controls.Add(this.LampYellow);
+            this.Controls.Add(this.LampRed);
+            this.Name = "Form1";
             this.Text = "Form1";
+
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LampRed;
+        private System.Windows.Forms.Button LampYellow;
+        private System.Windows.Forms.Button LampGreen;
     }
 }
 
--- Signal/Form1.cs
+++ Signal/Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Signal
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() //생성자함수 프로그램 실행시키자마자 실행
         {
             InitializeComponent();
+            Thread thread = new Thread(SignalTower);
+            thread.Start();
+        }
+
+
+        void SignalTower()
+        {
+            int D100 = 0;
+
+            while (true)
+            {
+                switch (D100)
+                {
+                    case 0:
+                        LampRed.BackColor = Color.Red;
+                        LampYellow.BackColor = SystemColors.Control;
+                        LampGreen.BackColor = SystemColors.Control;
+                        Thread.Sleep(1000);
+                        D100 = 100;
+                        break;
+
+
+                    case 100:
+                        LampRed.BackColor = SystemColors.Control;
+                        LampYellow.BackColor = Color.Yellow;
+                        LampGreen.BackColor = SystemColors.Control;
+                        Thread.Sleep(1000);
+                        D100 = 200;
+                        break;
+
+
+                    case 200:
+                        LampRed.BackColor = SystemColors.Control;
+                        LampYellow.BackColor = SystemColors.Control;
+                        LampGreen.BackColor = Color.Green;
+                        Thread.Sleep(1000);
+                        D100 = 0;
+                        break;
+
+                    default:
+                        D100 = 0;
+                        break;
+                }
+            }
         }
     }
 }
